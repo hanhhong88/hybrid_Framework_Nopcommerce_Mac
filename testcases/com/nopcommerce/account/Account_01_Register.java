fcff37c2b6d1cc70c4a9f0f9f18aea8bb218ff90
hanhhong88 <hanh.nguyenthihong88@gmail.com>
2023-04-22 21:06:46 +0700

    Create new repository

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,38 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Account_01_Register {
+	
+  @BeforeClass
+  public void beforeClass() {
+  }
+	
+
+  @Test
+  public void Register_01_Empty() {
+  }
+  
+  @Test
+  public void Register_02_InvalidEmail() {
+  }
+  
+  @Test
+  public void Register_03_InvalidPassword() {
+  }
+  
+  @Test
+  public void Register_04_InvalidConfirmPassword() {
+  }
+  
+  @Test
+  public void Register_05_ValidData() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
