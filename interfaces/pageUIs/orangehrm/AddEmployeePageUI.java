86f6c02e7a8b492c1ef1ff900add7269ad0db356
hanhhong88 <hanh.nguyenthihong88@gmail.com>
2023-12-16 21:37:09 +0700

    update

--- interfaces/pageUIs/orangehrm/AddEmployeePageUI.java
+++ interfaces/pageUIs/orangehrm/AddEmployeePageUI.java
@@ -4,7 +4,7 @@ public class AddEmployeePageUI {
 	public static final String FISTNAME_TBX = "css= input[name='firstName']";
 	public static final String LASTNAME_TBX = "css= input[name='lastName']";
 	public static final String EMPLOYEE_ID = "xpath= //label[text()='Employee Id']//parent::div//following-sibling::div/input";
-	public static final String SAVE_BTN = "xpath= //button[@type='submit']";
+	public static final String SAVE_BTN = "css= button[type='submit']";
 	
 	//button[contains(string(),'Save')]
 }
